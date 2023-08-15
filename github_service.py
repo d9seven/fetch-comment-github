e9f82ef0c3d5511d441412de0c20d2f6706c966b
ducpv-1644 <pham.van.duc-b@sun-asterisk.com>
2023-08-15 15:25:42 +0700

    add-create-excel

--- github_service.py
+++ github_service.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import re
+import xlsxwriter
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -16,11 +17,37 @@ class Github:
         self.since = os.environ.get("SINCE")
         self.header = {"Authorization": f"Bearer {self.token}"}
         self.base_url = self.get_base_url()
+        self.ignore_author = self.get_ignore_author()
+
+    def get_ignore_author(self):
+        ignore_author = os.environ.get("IGNORE_AUTHOR").split(',')
+        if "" in ignore_author:
+            ignore_author.remove("")
+        return ignore_author
 
     def get_base_url(self):
         url = f"https://api.github.com/repos/{self.owner}/{self.repo}/pulls/comments?since={self.since}"
-        url += "&per_page=100&sort=created&direction=desc"
+        url += "&per_page=100&sort=created&direction=asc"
         return url
+
+    def create_excel(self, data):
+        workbook = xlsxwriter.Workbook('result_comments.xlsx')
+        worksheet = workbook.add_worksheet()
+
+        col = 0
+
+        worksheet.write(0, col, "Author")
+        worksheet.write(0, col + 1, "Comment content")
+        worksheet.write(0, col + 2, "Date")
+        worksheet.write(0, col + 3, "PullRequest")
+        worksheet.write(0, col + 4, "Comment URL")
+        for idx, comment in enumerate(data):
+            worksheet.write(idx + 1, col, comment["author"])
+            worksheet.write(idx + 1, col + 1, comment["body"])
+            worksheet.write(idx + 1, col + 2, comment["date"])
+            worksheet.write(idx + 1, col + 3, comment["pr_id"])
+            worksheet.write(idx + 1, col + 4, comment["pr_link"])
+        workbook.close()
 
     def pr_review_comments(self):
         data = []
@@ -35,15 +62,16 @@ class Github:
             if not comments:
                 break
             for comment in comments:
+                author = comment["user"]["login"]
+                if author in self.ignore_author:
+                    continue
                 pr_link = comment['_links']["pull_request"]["href"]
                 data.append({
-                    "author": comment["user"]["login"],
+                    "author": author,
                     "body": comment["body"],
                     "date": comment["created_at"],
                     "pr_id": re.findall(r'\d+$', pr_link)[0],
                     "pr_link": pr_link,
                 })
             page += 1
-
-        for dt in data:
-            print(dt)
+        self.create_excel(data)
